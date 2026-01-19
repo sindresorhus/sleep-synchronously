45af766e4a8f80941c2431b4e1977c4c691e1d5a
Sindre Sorhus <sindresorhus@gmail.com>
2026-01-19 18:03:47 +0100

    Tweaks
    Closes #5

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const buffer = new Int32Array(new SharedArrayBuffer(4));
+
 export default function sleepSynchronously(milliseconds) {
-	Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, milliseconds);
+	if (milliseconds <= 0) {
+		return;
+	}
+
+	Atomics.wait(buffer, 0, 0, milliseconds);
 }
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,4 @@
 import {expectType} from 'tsd';
 import sleepSynchronously from './index.js';
 
-// eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
 expectType<void>(sleepSynchronously(200));
